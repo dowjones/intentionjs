1b1c50e48ede6635c9d26828ec24bc0bf7f433be
joe8756438 <joe@megazinemagazine.com>
2013-02-20 16:48:42 -0500

    updating some example files to work with the new intention

--- example/page.js
+++ example/page.js
@@ -1,7 +1,30 @@
 (function(){
 
   define(['intention'], function(intention) {
-		new intention();
+		
+
+
+		var tn = new intention,
+			resizeContexts = [{name:'standard', min:769}, 
+		        {name:'tablet', min:321},
+		        {name:'mobile', min:0}],
+
+		    hResponder = tn.responsive(resizeContexts,
+		        // compare the return value of the callback to each context
+		        // return true for a match
+		        function(test, context){
+		          if(test>=context.min){
+		            return true;
+		          }
+		        },
+		        // callback, return value is passed to matcher()
+		        // to compare against current context
+		        function(e){
+		          return $(window).width();
+		    	}),
+		    baseResponder = tn.responsive([{name:'base'}])('base');
+
+		$(window).on('resize', hResponder);
 	});
 
 })();
