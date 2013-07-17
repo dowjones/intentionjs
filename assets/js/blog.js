d576aa55cdcbbb51e106a83498d76982d29ffac2
Tyler Paige <tylerclarkpaige@gmail.com>
2013-07-17 18:24:21 -0400

    Fixed left-side tag navigation
    Wasn't a timing error, it was a search error. Fixed and working again.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,22 @@
 var buildBlog = function() {
-	var posts = [],
-		titlePos = [],
-		i = 1;
-	$.each($('#content section.header'), function() { 
-		var list = $(this).children('.tags').children('a'),
-			tags = [],
-			pos = $(this).position().top + 10;
-		if(i != 1) { pos += (20); } //position() doesn't account for padding
-		var markup = '<li id="a'+i+'" style="top:'+pos+'px"><div class="label"></div><div class="circle"></div></li>';
-		$('#content nav ul').append(markup);
-		$.each(list, function() {
-			var tag = $(this).text(),
-				linkMarkup = ' <a href="tagged/'+tag+'.html" class="'+tag+'">'+tag+'</a>';
-			tags.push(tag);
-			$('#a'+i+' .label').append(linkMarkup);
+		var posts = [],
+			titlePos = [],
+			i = 1;
+		$.each($('#content article'), function() { 
+			var list = $(this).children('section.header').children('.tags').children('a'),
+				tags = [],
+				pos = $(this).position().top + 10;
+			if(i != 1) { pos += (20); } //position() doesn't account for padding
+			console.log(i, pos);
+			var markup = '<li id="a'+i+'" style="top:'+pos+'px"><div class="label"></div><div class="circle"></div></li>';
+			$('#content nav ul').append(markup);
+			$.each(list, function() {
+				var tag = $(this).text(),
+					linkMarkup = ' <a href="tagged/'+tag+'.html" class="'+tag+'">'+tag+'</a>';
+				tags.push(tag);
+				$('#a'+i+' .label').append(linkMarkup);
+			});
+			posts.push(tags);
+			i++;
 		});
-		posts.push(tags);
-		i++;
-	});
 };
