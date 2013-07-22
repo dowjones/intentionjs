f47722bacee1bd0919a7faadc70c33448244ba8a
Tyler Paige <tylerclarkpaige@gmail.com>
2013-07-22 15:36:47 -0400

    Left side navigation is now touch responsive
    Renamed rewrite.js to base.js to make more sense/be more consistent.
    
    Added .PNG fallbacks for the device icons for IE8

--- assets/js/base.js
+++ assets/js/base.js
@@ -27,31 +27,42 @@ $(window).load(function() {
 			D = (D.clientHeight)? D: B;
 	}
 	
-	$(document).ready(function() { //Things that need the page to be done before running
-		contentPos = $('#content').offset().top + 3;
-		if(docsTest == true) { buildHome(contentPos, D); }
-		else { buildBlog(contentPos); }
-	})
-	.on('scroll', function(){
-		if(typeof pageYOffset == 'undefined') { var scroll = D.scrollTop; }
-		else { var scroll = pageYOffset; }
-		if(scroll >= contentPos){ //Get rid of nav instructions
-			window.setTimeout(function() {
-				$('#content nav').children('h6').fadeOut(500);
-			}, 1000);
-		}
-	});;
+	$(document)
+		.ready(function() { //Things that need the page to be done before running
+			contentPos = $('#content').offset().top + 3;
+			if(docsTest == true) { buildHome(contentPos, D); }
+			else { buildBlog(contentPos); }
+		})
+		.on('scroll', function(){
+			if(typeof pageYOffset == 'undefined') { var scroll = D.scrollTop; }
+			else { var scroll = pageYOffset; }
+			if(scroll >= contentPos){ //Get rid of nav instructions
+				window.setTimeout(function() {
+					$('#content nav').children('h6').fadeOut(500);
+				}, 1000);
+			}
+		});
+	
 	//Functions for opening the documentation nav (.active is open)
+	var oddClick = true;
 	$('#content nav')
 		.one('mouseenter', function() { $(this).children('h6').fadeOut(500); })
 		.on('mouseenter', function() { $(this).addClass('active'); })
-		.on('swiperight', function() { $(this).addClass('active'); })
 		.on('mouseleave', function() {
 			window.setTimeout(function() {
 				$('#content nav').removeClass('active');
 			}, 500);
 		})
-		.on('swipeleft', function() { $('#content nav').removeClass('active'); });
+		.on('tap', function() { //for mobile browser with back/forth swipe features
+			if(oddClick == true) { $(this).addClass('active'); }
+			else { $(this).removeClass('active'); }
+			oddClick = !oddClick;
+		});
+	$('#content')
+		.one('swipe', function() { $(this).children('nav').children('h6').fadeOut(500); })
+		.on('swiperight', function() { $('#content nav').addClass('active'); })
+		.on('swipeleft', function() { $('#content nav').removeClass('active'); })
+		.not('#content nav').on('tap', function() { $('#content nav').removeClass('active'); });
 		
 	//Layout organizing
 	intent
