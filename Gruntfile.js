b55e8b8eef6a61485cb63055e733264cf3dfc477
1800joe <call@1800joe.com>
2013-07-24 18:22:53 -0400

    adding grunt automation

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+//
+module.exports = function(grunt){
+  //grunt.loadNpmTasks('grunt-contrib-jshint');
+  //grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify:{
+      intention : {
+        options:{
+          banner:banner: '/*! <%= pkg.name %> v<%= pkg.version %>\
+\n* http://intentionjs.com/\
+\n*\
+\n* Copyright 2011, <%= grunt.template.today("yyyy") %>\ Dowjones and other contributors\
+\n* Released under the MIT license\
+\n*\
+\n*/\n'
+         },
+         files: {
+           'code/intention.min.js': ['intention.js']
+         }
+       },
+       context: {
+         options:{
+           banner: '/*! <%= pkg.name %> v<%= pkg.version %>\
+\n* http://intentionjs.com/\
+\n*\
+\n* Copyright 2011, <%= grunt.template.today("yyyy") %>\ Dowjones and other contributors\
+\n* Released under the MIT license\
+\n*\
+\n*/\n'
+         },
+         files: {
+
+           'code/context.min.js':['context.js']
+        }
+      },
+      combo: {
+        options:{
+          banner: '/*! <%= pkg.name %> v<%= pkg.version %>\
+\n* http://intentionjs.com/\
+\n*\
+\n* Copyright 2011, <%= grunt.template.today("yyyy") %>\ Dowjones and other contributors\
+\n* Released under the MIT license\
+\n*\
+\n*/\n'
+         },
+        files: {
+          'code/in.context.min.js': ['intention.js', 'context.js']
+        }
+      }
+    }
+  });
+}
