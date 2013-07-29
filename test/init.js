4124b4f2906f8da9c40901d4e1840b898fbf0391
1800joe <call@1800joe.com>
2013-07-29 12:17:13 -0400

    adding the init deferred example for tyler to test

---
+++ test/init.js
@@ -0,0 +1,26 @@
+function in_init(contexts, callback){
+  var dfds = [];
+
+  _.each(contexts, function(ctx){
+    var dfd = $.Deferred();
+
+    dfds.push(dfd);
+
+    if(intent.is(ctx)) {
+      dfd.resolve();
+    } else {
+      intent.on(ctx, dfd.resolve);
+    }
+  });
+
+  if((!contexts) || (contexts.length === 0)){
+    sheetWriter();
+  }
+  $.when.apply(this, dfds).done(callback);
+
+};
+
+//usage:
+in_init(['standard'], function(){
+  console.log('standard has been entered');
+});
