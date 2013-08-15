b2543887b506df738d21d8d581bbc7f1cc9115b4
1800joe <call@1800joe.com>
2013-08-15 15:11:55 -0400

    reshuffling and organizing everything also getting rid of evil tabs

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,27 +1,26 @@
 var buildBlog = function() {
-		var posts = [],
-			i = 1;
-		$(window).on('scroll', function() { //first setup listeners
-			//Gotta reset these for IE8
-			if(typeof pageYOffset == 'undefined') { var scroll = D.scrollTop; }
-			else { var scroll = pageYOffset; }
-			
-			//Fixing the docsNav position
-		});
-		$.each($('#content article'), function() { 
-			var list = $(this).children('section.header').children('.tags').children('a'),
-				tags = [];
-			$(this).attr('id', 't'+i);
-			var markup = '<li id="a'+i+'"><div class="label"></div><div class="circle"></div></li>';
-			$('#content nav ol').append(markup);
-			$.each(list, function() {
-				var tag = $(this).text(),
-					linkMarkup = ' <a href="/blog/tagged/'+tag+'.html" class="'+tag+'">'+tag+'</a>';
-				tags.push(tag);
-				$('#a'+i+' .label').append(linkMarkup);
-			});
-			$('#a'+i+' .label').append('<br/><a class="jump" href="#t'+i+'">Jump</a>');
-			posts.push(tags);
-			i++;
-		});
+  var posts = [],
+  i = 1;
+  $(window).on('scroll', function() { //first setup listeners
+    //Gotta reset these for IE8
+    if(typeof pageYOffset == 'undefined') { var scroll = D.scrollTop; }
+    else { var scroll = pageYOffset; }
+    //Fixing the docsNav position
+  });
+  $.each($('#content article'), function() {
+    var list = $(this).children('section.header').children('.tags').children('a'),
+    tags = [];
+    $(this).attr('id', 't'+i);
+    var markup = '<li id="a'+i+'"><div class="label"></div><div class="circle"></div></li>';
+    $('#content nav ol').append(markup);
+    $.each(list, function() {
+      var tag = $(this).text(),
+      linkMarkup = ' <a href="/blog/tagged/'+tag+'.html" class="'+tag+'">'+tag+'</a>';
+      tags.push(tag);
+      $('#a'+i+' .label').append(linkMarkup);
+    });
+    $('#a'+i+' .label').append('<br/><a class="jump" href="#t'+i+'">Jump</a>');
+    posts.push(tags);
+    i++;
+  });
 };
