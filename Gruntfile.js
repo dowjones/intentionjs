6af9bf53170f0d4d7f63151f6177e5d54cd7a77f
1800joe <call@1800joe.com>
2013-09-02 20:03:08 -0400

    adding a few modifications to the grunt file, including grunt-release also building the distribution files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,15 @@ module.exports = function(grunt){
   //grunt.loadNpmTasks('grunt-contrib-jshint');
   //grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-release');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    release: {
+      options:{
+        npm:false
+      }
+    },
     uglify:{
       intention : {
         options:{
@@ -39,23 +45,9 @@ module.exports = function(grunt){
 
            'code/context.min.js':['context.js']
         }
-      },
-      combo: {
-        options:{
-          banner: '/*! <%= pkg.name %> v<%= pkg.version %>\
-\n* http://intentionjs.com/\
-\n*\
-\n*\ intention.js + context.js\
-\n*\
-\n* Copyright 2011, <%= grunt.template.today("yyyy") %>\ Dowjones and other contributors\
-\n* Released under the MIT license\
-\n*\
-\n*/\n'
-         },
-        files: {
-          'code/in.context.min.js': ['intention.js', 'context.js']
-        }
       }
     }
   });
+
+
 }
